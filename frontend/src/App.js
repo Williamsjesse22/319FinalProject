3a0c195007c2ca8426f1bb9555a2b418ef7ff82b
Jesse Williams <Jwills22@iastate.edu>
2024-12-09 17:26:14 -0600

    merge conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,3 @@
-<<<<<<< Updated upstream
-// App.js
-import React, { useState } from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import AddPet from "./AddPet";
-import DeletePet from "./DeletePets";
-import Pets from "./Pets"; // Import your Pets component
-import SearchPet from "./SearchPets";
-import Sidebar from "./Sidebar";
-
-const App = () => {
-  const [refresh, setRefresh] = useState(false);
-
-  const refreshPets = () => {
-    setRefresh((prev) => !prev);
-  };
-
-  return (
-    <Router>
-      <div className="d-flex">
-        <Sidebar />
-        <div className="flex-grow-1 p-3">
-          <Routes>
-            <Route
-              path="/"
-              element={<h1>Welcome to the Pet Adoption App</h1>}
-            />
-            <Route path="/listPets" element={<Pets />} />{" "}
-            {/* Display Pets component here */}
-            <Route path="/searchPet" element={<SearchPet />} />
-            <Route
-              path="/addPet"
-              element={<AddPet refreshPets={refreshPets} />}
-            />
-            <Route
-              path="/deletePet"
-              element={<DeletePet refreshPets={refreshPets} />}
-            />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
-=======
 import React, { useState } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import AddPet from './AddPet';
@@ -86,7 +42,6 @@ const App = () => {
 			</div>
 		</Router>
 	);
->>>>>>> Stashed changes
 };
 
 export default App;
